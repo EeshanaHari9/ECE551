4a01908c0ecfabb3d54ee243c6bee0aa6a20d398
sodonne6 <odonnellsd1@gmail.com>
2025-04-22 12:18:00 -0500

    Update telemetry.sv
    working telemetry

--- project/Controller/telemetry.sv
+++ project/Controller/telemetry.sv
@@ -1,126 +1,107 @@
-// telemetry.sv
-// 
-// Dustin Nguyen (danguyen2@wisc.edu)
-// E C E 551
+module telemetry (batt_v, avg_curr, avg_torque, clk, rst_n, TX);
 
-module telemetry(
-    input wire clk,
-    input wire rst_n,
-    input wire [11:0] batt_v,
-    input wire [11:0] avg_curr,
-    input wire [11:0] avg_torque,
-    output logic TX,
-    output logic tx_done
-);
+ input [11:0] batt_v;
+ input [11:0] avg_curr;
+ input [11:0] avg_torque;
+ input clk, rst_n;
+ output TX;
 
-    logic [7:0] tx_data;
-    logic trmt;
-    logic uart_tx_done;
-    logic [3:0] packet;
+//-----intialize the uart_tx-----------------------------------
+ logic trmt;
+ logic [7:0] tx_data;
+ logic tx_done;
 
-    UART_tx uart(
-        .clk(clk), .rst_n(rst_n),
-        .TX(TX), .trmt(trmt), .tx_data(tx_data), .tx_done(uart_tx_done)
-    );
+ UART_tx U_tx(.clk(clk), .rst_n(rst_n), .TX(TX), .trmt(trmt), .tx_data(tx_data), .tx_done(tx_done));
 
-    typedef enum logic [2:0] { IDLE, SEND, DONE } state_t;
+//----packet-----------------------------------------------------
+ logic [7:0] delim1, delim2;
+ logic [7:0] payload1, payload2, payload3, payload4, payload5, payload6;
+ logic [63:0] data; //the whole 8 bytes data
 
-    state_t state, nxt_state; // Current and next state variables
-    logic [4:0] timer; // 5-bit timer (used for pacing transmissions)
-    
-    // 5-bit timer to pace transmissions
-    always_ff @ (posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-            timer <= 0;
-        end else begin
-            timer <= timer + 1; // Increment timer every clock cycle
-            if (timer[4]) begin
-                timer <= 0; // Reset timer when the MSB is asserted
-            end
-        end
-    end
+ assign delim1 = 8'hAA;
+ assign delim2 = 8'h55;
+ assign payload1 = {4'h0, batt_v[11:8]};
+ assign payload2 = batt_v[7:0];
+ assign payload3 = {4'h0, avg_curr[11:8]};
+ assign payload4 = avg_curr[7:0];
+ assign payload5 = {4'h0, avg_torque[11:8]};
+ assign payload6 = avg_torque[7:0];
 
-    // Packet Selection for transmission
-    always_comb begin
-        case (packet)
-            4'd1: begin
-                tx_data = 8'hAA;
-            end
-            4'd2: begin
-                tx_data = 8'h55;
-            end
-            4'd3: begin
-                tx_data = {4'h0, batt_v[11:8]};
-            end
-            4'd4: begin
-                tx_data = batt_v[7:0];
-            end
-            4'd5: begin
-                tx_data = {4'h0, avg_curr[11:8]};
-            end
-            4'd6: begin
-                tx_data = avg_curr[7:0];
-            end
-            4'd7: begin
-                tx_data = {4'h0,avg_torque[11:8]};
-            end
-            4'd8: begin
-                tx_data = avg_torque[7:0];
-            end
-            default begin
-                tx_data = 8'h00;
-            end
-        endcase 
-    end
+//----counter for clock----------------------------
+//50M/47.68=2^20, so the counter should be 20 bits
+ logic [19:0] cnt_clk;
+ logic full_clk;
 
-    // State machine
-    always @(posedge clk, negedge rst_n) begin
-        if (!rst_n) begin
-            state <= IDLE; // Reset state to IDLE
-        end else begin
-            state <= nxt_state; // Transition to the next state
-        end;
-    end
+ always_ff @( posedge clk, negedge rst_n)begin
+    if (!rst_n) 
+       cnt_clk <= 20'b0;
+    else 
+       cnt_clk <= cnt_clk + 1;
+ end
 
-    // flop trmt
+ assign full_clk = &cnt_clk;
 
-    always_comb begin
-        trmt = 0;
-        tx_done = 0;
-        
-        case (state)
-            IDLE: begin
-                if (timer[4]) begin // Waits for the timer to expire before initiating transmission.
-                    trmt = 1; // Start transmission
-                    nxt_state = SEND;
-                end else begin
-                    packet = 0; // Reset packet counter to send 0 data
-                    nxt_state = IDLE;
-                end
-            end
-            // Sends data and waits for transmission to complete.
-            SEND: begin
-                if (uart_tx_done) begin
-                    trmt = 0; // Stop transmission
-                    nxt_state = DONE;
-                end else begin
-                    nxt_state = SEND;
-                end
-            end
-            // Moves to the next packet or resets the process.
-            DONE: begin
-                tx_done = 1;
-                if (packet == 4'b1001) begin // If the packet counter reaches its max, return to IDLE.
-                    packet = 0;
-                    trmt = 0;
-                    nxt_state = IDLE;
-                end else begin
-                    packet = packet + 1; // Move to next packet
-                    trmt = 1; // Start new transmission
-                    nxt_state = SEND;
-                end
-            end
-        endcase
-    end
+//----shifter reg used to transmit data (byte)-----
+//8 bytes, so the counter will be 3 bits
+ logic [2:0] cnt_shft;
+ logic clr, shft;//clr--clear the counter, shft--shft 1 byte when assert
+ logic full_shft;
+ 
+ always_ff @( posedge clk, negedge rst_n)begin
+   if (!rst_n) begin
+     cnt_shft <= 3'b000;
+     data <= {delim1, delim2, payload1, payload2, payload3, payload4, payload5, payload6};//asynch reset
+   end else if (clr) begin
+     cnt_shft <= 3'b000;
+     data <= {delim1, delim2, payload1, payload2, payload3, payload4, payload5, payload6};//enable signal
+   end else if (shft) begin
+     cnt_shft <= cnt_shft + 1;
+     data <= {data[55:00], data[63:56]};//rotate left, and tx will be the MSbyte
+   end else begin
+     cnt_shft <= cnt_shft;
+     data <= data;//hold the state
+   end
+ end
 
-endmodule;
+ assign full_shft = &cnt_shft;
+ assign tx_data = data[63:56];
+
+//----SM---------------------------------------
+typedef enum reg [1:0] {BEGIN, WAIT, IDLE} state_t;
+state_t state, nxt_state;
+
+ always_ff @(posedge clk, negedge rst_n)
+   if (!rst_n)
+       state <= BEGIN;
+   else	
+       state <= nxt_state;//asynch reset
+
+ always_comb begin
+   clr = 0;
+   shft = 0;
+   trmt = 0;
+  case(state)
+       BEGIN: begin
+	   nxt_state = WAIT;
+	   trmt = 1;//hold trmt high when want to transmit data_tx
+       end
+       WAIT: begin 
+	     if (full_shft)
+		 nxt_state = IDLE;//all 8 bytes are transmitted
+	     else if (tx_done) begin
+                 nxt_state = BEGIN;
+		 shft = 1;//tx_done assert, start to shift 1 by 1 byte
+	     end else
+                 nxt_state = WAIT;//wait until tx_done assert
+	end
+	IDLE: begin
+	      if (full_clk) begin
+		clr = 1;
+		nxt_state = BEGIN;
+	      end else
+	        nxt_state = IDLE;
+	end
+	default: nxt_state = BEGIN;	
+   endcase
+ end
+endmodule
